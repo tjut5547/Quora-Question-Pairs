f0386c8ecdf8b67cf5a25548b2bc0fdf06bbd16b
tjut5547 <1021129753@qq.com>
2017-09-11 00:11:01 +0800

    修改代码bug ：梯度不能下降的问题，自己对交叉熵函数没有理解

--- cnn.py
+++ cnn.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 
+
 class Cnn(object):
     def __init__(self, sequence_length, vocab_size, embedding_size,
-                 filter_sizes, num_filters):
-        self.label = tf.placeholder(tf.bool, [None], name = "label")
-        self.input_sentence_a = tf.placeholder(tf.int32, [None, sequence_length], name = "input_a")
-        self.input_sentence_b = tf.placeholder(tf.int32, [None, sequence_length], name = "input_b")
+                 filter_sizes, num_filters, num_classes):
+        self.label = tf.placeholder(tf.bool, [None, num_classes], name="label")
+        self.input_sentence_a = tf.placeholder(tf.int32, [None, sequence_length], name="input_a")
+        self.input_sentence_b = tf.placeholder(tf.int32, [None, sequence_length], name="input_b")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
         l2_loss = tf.constant(0.0)
@@ -52,7 +53,7 @@ class Cnn(object):
                 pooled_outputs_a.append(pooled_a)
                 pooled_outputs_b.append(pooled_b)
 
-        num_filters_total = num_filters * len(filter_sizes) # 10 * ４
+        num_filters_total = num_filters * len(filter_sizes)
         self.h_pool_a = tf.concat(pooled_outputs_a, 3)
         self.h_pool_b = tf.concat(pooled_outputs_b, 3)
         self.h_pool_flat_a = tf.reshape(self.h_pool_a, [-1, num_filters_total])
@@ -67,26 +68,30 @@ class Cnn(object):
         '''
 
         with tf.name_scope("output"):
-            Wa = tf.Variable(tf.truncated_normal([None, num_filters_total], stddev=0.1), name="Wa")
-            Wb = tf.Variable(tf.truncated_normal([None, num_filters_total], stddev=0.1), name="Wa")
-            ba = tf.Variable(tf.constant(0.1, shape=[None]), name="ba")
-            bb = tf.Variable(tf.constant(0.1, shape=[None]), name="bb")
-            self.mul_a = tf.add(tf.multiply(Wa, self.h_pool_flat_a), ba)
-            self.mul_b = tf.add(tf.multiply(Wb, self.h_pool_flat_b), bb)
-            self.result = self.add(self.mul_a, self.mul_b)
+            Wa = tf.Variable(tf.truncated_normal(shape=[num_filters_total, num_classes], stddev=0.1), name="Wa")
+            Wb = tf.Variable(tf.truncated_normal(shape=[num_filters_total, num_classes], stddev=0.1), name="Wa")
+            ba = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="ba")
+            bb = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="bb")
+            self.mul_a = tf.nn.xw_plus_b(self.h_pool_flat_a, Wa, ba)
+            self.mul_b = tf.nn.xw_plus_b(self.h_pool_flat_b, Wb, bb)
+            self.result = tf.add(self.mul_a, self.mul_b)
 
+            print_all = [Wa, Wb, self.mul_a, self.mul_b, self.result]
+            for tensor in print_all:
+                print (tensor)
 
         with tf.name_scope("loss"):
-            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.result, labels=self.label)
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.result, labels=self.label))
 
 
-                # Accuracy
+        # Accuracy
         # with tf.name_scope("accuracy"):
         #     correct_predictions = tf.equal(self.label, tf.argmax(self.result, 1))
         #     self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
-#
+
 # cnn = Cnn(sequence_length=35,
 #           vocab_size=1000,
 #           embedding_size=50,
 #           filter_sizes=[1, 2, 3, 4, 5],
-#           num_filters=10)
+#           num_filters=10,
+#           num_classes=2)
--- train.py
+++ train.py
@@ -8,53 +8,59 @@ from cnn import *
 from preexe import *
 from tensorflow.contrib import learn
 
+import matplotlib.pyplot as plt
+
+global_loss = []
+
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(
-        allow_soft_placement = True,
-        log_device_placement = False
+        allow_soft_placement=True,
+        log_device_placement=False
     )
-    sess = tf.Session(config = session_conf)
+    sess = tf.Session(config=session_conf)
     with sess.as_default():
-        cnn = Cnn(sequence_length=40,
+        cnn = Cnn(sequence_length=50,
                   vocab_size=24000,
                   embedding_size=128,
-                  filter_sizes=[3, 4, 5],
-                  num_filters=10)
+                  filter_sizes=[2, 3, 4, 5],
+                  num_filters=128,
+                  num_classes=2)
         global_step = tf.Variable(0, name="global_step", trainable=False)
-        optimizer = tf.train.AdamOptimizer(0.0002).minimize(cnn.loss)
+        optimizer = tf.train.AdamOptimizer(0.0005).minimize(cnn.loss, global_step=global_step)
         # grads_and_vars = optimizer.compute_gradients(cnn.loss)
         # train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-        # global_step : 每一轮参数值的更新之后就会增加１
+        # global_step : 每一轮参数值的更新之后就会增加1
 
         sess.run(tf.global_variables_initializer())
+
         def train_step(a_batch, b_batch, label):
             feed_dict = {
-                cnn.input_sentence_a : a_batch,
-                cnn.input_sentence_b : b_batch,
+                cnn.input_sentence_a: a_batch,
+                cnn.input_sentence_b: b_batch,
                 cnn.label : label
             }
             _, step, loss = sess.run([optimizer, global_step, cnn.loss], feed_dict=feed_dict)
             time_str = datetime.datetime.now().isoformat()
             print("{}: step {}, loss {:g}".format(time_str, step, loss))
+            global_loss.append(loss)
 
         def dev_step(a_batch, b_batch, label):
             feed_dict = {
-                cnn.input_sentence_a : a_batch,
-                cnn.input_sentence_b : b_batch,
+                cnn.input_sentence_a: a_batch,
+                cnn.input_sentence_b: b_batch,
                 cnn.label : label
             }
-            step, loss = sess.run([global_step, cnn.loss], feed_dict = feed_dict)
+            step, loss = sess.run([global_step, cnn.loss], feed_dict=feed_dict)
             time_str = datetime.datetime.now().isoformat()
             print("{}: step {}, loss {:g}".format(time_str, step, loss))
 
-
         batches = get_batch(500)
         for data in batches:
-            print (datetime.datetime.now().isoformat())
+            print(datetime.datetime.now().isoformat())
             x_train, y_train = zip(*data)
             x_train_a = [a for a, b in x_train]
             x_train_b = [b for a, b in x_train]
-            y_train = [label[0] for label in y_train]
+            y_train = [((1 + label[0]) % 2, (0 + label[0]) % 2) for label in y_train]
 
             train_step(x_train_a, x_train_b, y_train)
             current_step = tf.train.global_step(sess, global_step)
@@ -62,3 +68,11 @@ with tf.Graph().as_default():
             #     print("\nEvaluation:")
             #     dev_step(x_train_a, x_train_b, y_train)#, writer=dev_summary_writer)
             #     print("")
+
+        x = list(range(len(global_loss)))
+        y = global_loss
+        print (y)
+        plt.plot(x, y, 'r', label="loss")
+        plt.xlabel("batches")
+        plt.ylabel("loss")
+        plt.savefig("loss_modify.png")
